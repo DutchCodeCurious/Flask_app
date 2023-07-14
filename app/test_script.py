85c3e12e232ee14259840a4380fae567ff85fa37
DutchCodeCurious <114599908+DutchCodeCurious@users.noreply.github.com>
2023-07-14 14:38:03 +0200

    updated test'

--- app/test_script.py
+++ app/test_script.py
@@ -1,13 +1,4 @@
-#def test_handle(client):
-#    response = client.post('/', data={'input_text': 'Hello World'})
-#
-#    assert response.status_code == 200
-#    assert b'dlroW olleH' in response.data
-#    assert 'result.html' in response.template.name
-
-
 import unittest
-from flask import Flask
 from app import app
 
 
